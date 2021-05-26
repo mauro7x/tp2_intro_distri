c7519e8f8069244e7b0597b6f6077451a28f9fea
Mauro Parafati <mparafati@fi.uba.ar>
2021-05-26 04:41:37 -0300

    Seems to be working, progress bar is freezed tho

--- src/lib/stop_and_wait.py
+++ src/lib/stop_and_wait.py
@@ -61,11 +61,15 @@ class StopAndWait(RDTInterface):
         TODO: docs.
         """
 
+        logger.debug(f'[saw:send] == START SENDING ==')
+        logger.debug(f'[saw:send] Data to send: {total_data}')
+
         for i in range(0, len(total_data), DATA_SIZE):
             # We divide total data in segments of DATA_SIZE
             data = total_data[i:(i + DATA_SIZE)]
             datagram = self.seq_num + data
 
+            logger.debug(f'[saw:send] Sending datagram ({datagram})...')
             send_datagram(datagram, self._send)
             start = now()
 
@@ -75,13 +79,23 @@ class StopAndWait(RDTInterface):
                     # We wait for the ack to arrive
                     ack, _ = _split(recv_datagram(self._recv, TIMEOUT, start))
                     datagram_ackd = (ack == self.seq_num)
+                    if datagram_ackd:
+                        logger.debug(
+                            f'[saw:send] Good ACK received.')
+                    else:
+                        logger.debug(
+                            f'[saw:send] Wrong ACK received ({ack}, expected {self.seq_num}).')
+
                 except SocketTimeout:
                     # If recv timed out, we re-send the chunk
+                    logger.debug(
+                        f'[saw:send] Timed out. Re-sending datagram ({datagram})...')
                     send_datagram(datagram, self._send)
                     start = now()
 
             self.seq_num = self._get_next(self.seq_num)
 
+        logger.debug(f'[saw:send] == FINISH SENDING ==')
         return
 
     def recv(self, length):
@@ -89,25 +103,39 @@ class StopAndWait(RDTInterface):
         TODO: docs.
         """
 
+        logger.debug(f'[saw:recv] == START RECEIVING ==')
+        logger.debug(f'[saw:recv] Length: {length}')
+
         # TODO: Add global timer so it doesn't block 4ever?
 
         result = []
         total_recd = 0
 
         while total_recd < length:
-            ack, data = _split(recv_datagram(self._recv))
+            sn, data = _split(recv_datagram(self._recv))
 
             # If acks dont't match we re-send the last ack
-            if ack != self.seq_ack:
+            if sn != self.seq_ack:
+                logger.debug(
+                    f'[saw:recv] Wrong SN received ({sn}). Re-sending last ackd SN ({self.seq_ack})...')
                 send_datagram(self._get_prev(self.seq_ack), self._send)
                 continue
 
             # If acks match, we keep the data and continue
+            logger.debug(
+                f'[saw:recv] Good SN received. Data received: ({data})')
             left = length - total_recd
             result.append(data[:left])
             total_recd += len(data)
 
+            logger.debug(
+                f'[saw:recv] Sending ack ({self.seq_ack})...')
             send_datagram(self.seq_ack, self._send)
             self.seq_ack = self._get_next(self.seq_ack)
 
-        return b''.join(result)
+        result = b''.join(result)
+
+        logger.debug(f'[saw:recv] Total data received: {result}')
+        logger.debug(f'[saw:recv] == FINISH RECEIVING ==')
+
+        return result
