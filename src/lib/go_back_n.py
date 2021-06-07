dce4e7e54a33e0ec8430b341fbc7c918c0a904e7
TaielC <43725160+TaielC@users.noreply.github.com>
2021-06-06 23:27:16 -0300

    The idea has begun

--- src/lib/go_back_n.py
+++ src/lib/go_back_n.py
@@ -1,14 +1,31 @@
+from collections import deque
+
 # Lib
-from lib.rdt_interface import RDTInterface
+from lib.rdt_interface import RDTInterface, RecvCallback, SendCallback
+from lib.rtt_handler import RTTHandler
 
 
 class GoBackN(RDTInterface):
 
-    def __init__(self, _send, _recv, n=10) -> None:
-        pass
+    def __init__(self, _send: SendCallback, _recv: RecvCallback,
+                 window_size: int = 10) -> None:
+        self.n = window_size
+        self._send_datagram = _send
+        self._recv_datagram = _recv
+        self.base = 0
+        self.next = 0
+        self.sn_send = 0
+        self.sn_recv = 0
+        self.rtt = RTTHandler()
+        return
 
-    def send(self, data):
-        pass
+    def send(self, data: bytearray):
+        base = 0
+
+        while base < len(data):
+            _send_window(base, )
+
+        return
 
     def recv(self, length):
         pass
